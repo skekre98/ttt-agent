b60a9010d7a11e438ad0e0779cf69eea74f5ec48
Sharvil Kekre <sharvilkekre@gmail.com>
2021-10-21 15:05:30 -0700

    fixed savePolicy method

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,21 @@
 using namespace std;
 
 int main() {
-	Agent p1 = Agent("Sharvil");
-	Human p2 = Human("Sharvil2");
-	Board board(p1, p2);
-	board.agentPlay(10000);
+	string train;
+	cout << "Would you like to train new TTT agents(y,n) ";
+	cin>>train;
+	if (train == "Y" || train == "y" || train == "yes") {
+		Agent agent1("AgentX");
+		Agent agent2("AgentO");
+		Board board(agent1, agent2);
+		int rounds;
+		cout << "How many rounds would you like to simulate: ";
+		cin>>rounds;
+		board.agentPlay(rounds);
+
+		Agent testAgent;
+		testAgent.loadPolicy("AgentO_policy.txt");
+		testAgent.showStateValues();
+	}
 	return 0;
 }
